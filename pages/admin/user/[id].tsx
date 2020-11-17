65046e98929ff4bee5ade47273f3eb35ef47c205
huynvn <hpn010193@gmail.com>
2020-11-18 00:48:56 +0700

    09 Tìm hiểu Routing Basic

--- pages/admin/user/[id].tsx
+++ pages/admin/user/[id].tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+
+// Demo Client Side Rendering.
 export default function AdminUser() {
+  const router = useRouter();
+  console.log("AdminUser -> router", router.query.id);
+
+  function handleOnClick() {
+    router.push('/login');
+  }
+
   return (
     <>
       <Head>
         <title>User detail</title>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></link>
       </Head>
-      <h1>Admin - User Page</h1>;
+      <h1>Admin - User Page {router.query.id} </h1>
+      <Link href="/login">
+        <a className="active">Go to homepage</a>
+      </Link>
+      <button onClick={handleOnClick}>Go to homepage</button>
     </>
   );
 }
