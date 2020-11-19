fa488317530ca3e3031c837abf2e31519753976d
huynvn <hpn010193@gmail.com>
2020-11-19 23:50:08 +0700

    10: Tim hieu static file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App Edit</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/css/global.css"/>
       </Head>
 
       <main className={styles.main}>
+        <img src="/images/image_01.jpg"/>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
