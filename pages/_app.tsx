55ab66acec094247dbacf99eeeb57c4a6a82eaaa
huynvn <hpn010193@gmail.com>
2020-12-24 00:14:37 +0700

    Lập trình NextJs - 23 Solution 01

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import "../assets/style.scss";
+import "../components/ModalSolution/modal.scss";
 
 // type AppProps = {
 //   isLogin: boolean;
