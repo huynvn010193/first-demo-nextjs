34cf0e0ddd950690f686fd26a8b6160c10831c69
huynvn <hpn010193@gmail.com>
2021-03-02 23:58:09 +0700

    done getinitalProps

--- pages/playground/getIntialProps/index.tsx
+++ pages/playground/getIntialProps/index.tsx
@@ -1,28 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import { type } from "os";
+import React, { useEffect, useState } from "react";
+import { NextPage, NextPageContext } from "next";
 
 const BASE_URL = "http://api-meme-zendvn-01.herokuapp.com/api";
 
-const DemoGetIntialProps = () => {
-  const [posts, setPosts] = useState([]);   
-  useEffect(() => {
-    fetch(BASE_URL + "/post/getListPagination.php?pagesize=10&currPage=1")
-      .then(async response => {
-        const data = await response.json();
-        console.log("data", data.posts);
-        setPosts(data.posts);
-      }) // chuyển đổi res về dạng json.
-  },[]);
+type PostType = {
+  PID: string;
+  post_content: string;
+};
+
+type PropsType = {
+  posts: PostType[];
+};
+
+const DemoGetIntialProps: NextPage<PropsType> = ({ posts }) => {
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   fetch(BASE_URL + "/post/getListPagination.php?pagesize=10&currPage=1").then(
+  //     async (response) => {
+  //       const data = await response.json();
+  //       console.log("data", data.posts);
+  //       setPosts(data.posts);
+  //     }
+  //   ); // chuyển đổi res về dạng json.
+  // }, []);
 
   return (
     <div className="container">
       <h1>Demo GetIntialProps</h1>
       <ul>
-        {
-          posts.map((posts) =><li key={posts.PID}>{posts.post_content}</li>)
-        }
+        {posts.map((posts) => (
+          <li key={posts.PID}>{posts.post_content}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
+
+DemoGetIntialProps.getInitialProps = async (context: NextPageContext) => {
+  const response = await fetch(
+    BASE_URL + "/post/getListPagination.php?pagesize=10&currPage=1"
+  );
+  const data = await response.json();
+  console.log("data", data);
+  return {
+    posts: data.posts,
+  };
+};
 
 export default DemoGetIntialProps;
