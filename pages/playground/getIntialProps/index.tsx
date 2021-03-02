3eba020abe282099233ccaf6f8773c9d77480957
huynvn010193 <hpn010193@gmail.com>
2021-03-02 08:40:04 +0700

    Done clip Fetch-data

--- pages/playground/getIntialProps/index.tsx
+++ pages/playground/getIntialProps/index.tsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BASE_URL = "http://api-meme-zendvn-01.herokuapp.com/api";
 
-const DemoGetIntialProps = () => {  
+const DemoGetIntialProps = () => {
+  const [posts, setPosts] = useState([]);   
   useEffect(() => {
-    fetch(BASE_URL + "/post/getListPagination.php?pagesize=3&currPage=1")
-      .then(response => response.json())
-      .then(data => console.log(data.posts));
+    fetch(BASE_URL + "/post/getListPagination.php?pagesize=10&currPage=1")
+      .then(async response => {
+        const data = await response.json();
+        console.log("data", data.posts);
+        setPosts(data.posts);
+      }) // chuyển đổi res về dạng json.
   },[]);
 
   return (
     <div className="container">
       <h1>Demo GetIntialProps</h1>
+      <ul>
+        {
+          posts.map((posts) =><li key={posts.PID}>{posts.post_content}</li>)
+        }
+      </ul>
     </div>
   )
 }
